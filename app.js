08e0b37fd2dc1c826f3dd6a3001dd8f9b087b177
Utkarsh <utkarsh.yadav5@gmail.com>
2017-07-06 01:36:29 +0530

    Emitting message functionality added

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var socket = require('socket.io');
+
+//App setup
 var app = express();
 
-//static file
+//static files
 app.use(express.static('public'));
 
-app.listen('3000');
+var server = app.listen('3000');
 console.log('Listening to port');
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection',function(socket){
+    console.log('made socket connection',socket.id);
+
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data);
+    });
+});
