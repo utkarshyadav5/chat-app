2c528b7386a58e23181e3835c5b334196ec49b1a
Utkarsh <utkarsh.yadav5@gmail.com>
2017-07-06 01:50:11 +0530

    Added broadcasting functionality

--- public/chat.js
+++ public/chat.js
@@ -7,6 +7,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 //Emit events
 btn.addEventListener('click',function(){
@@ -16,7 +17,17 @@ btn.addEventListener('click',function(){
     });
 });
 
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handle.value);
+});
+
+
 //Listen for events
 socket.on('chat',function(data){
+    feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
+
+socket.on('typing',function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+})
